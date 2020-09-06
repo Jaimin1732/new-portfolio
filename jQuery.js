5e23caa3034a3cfab661d252edd63152d2dfd63f
Jaimin1732 <Jaimin182004.rp@gmail.com>
2020-09-06 14:09:57 -0400

    Few more jquery updates

--- jQuery.js
+++ jQuery.js
@@ -1,1 +1,7 @@
-$(selector).fadeIn();
+$(document).ready(function () {
+    $("#landing").slideToggle(700);
+    $("#nav").show(700);
+    $('#portfolio').slideToggle(700);
+    $('#first-website').fadeIn(900);
+    $('#second-website').fadeIn(900);
+  });
