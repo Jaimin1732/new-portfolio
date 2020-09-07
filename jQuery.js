552deb8e58d5214804f57b4d0d115376bd1f2d9a
Jaimin1732 <Jaimin182004.rp@gmail.com>
2020-09-06 20:45:48 -0400

    Few more changes rolling in

--- jQuery.js
+++ jQuery.js
@@ -2,6 +2,6 @@ $(document).ready(function () {
     $("#landing").slideToggle(700);
     $("#nav").show(700);
     $('#portfolio').slideToggle(700);
-    $('#first-website').fadeIn(900);
-    $('#second-website').fadeIn(900);
   });
+
+  AOS.init();
